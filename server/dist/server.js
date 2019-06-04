70f583b894acb128e26aef381155fa3cf8c9b0ee
Jonathan Barcela <jonathan.barcela@viaflow.com.br>
2019-06-04 15:52:54 -0300

    Image recognition and text extract using Google Vision and Google Language

--- server/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var image_controller_1 = require("./controllers/image.controller");
-var app = new app_1.default([
-    new image_controller_1.default(),
-], 5000);
-app.listen();
-//# sourceMappingURL=server.js.map
